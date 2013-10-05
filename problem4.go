d6f9ed0ec32b9d54d1279b2b03820eb5282feccf
Aditya Mukerjee <dev@chimeracoder.net>
2013-10-05 15:18:47 -0400

    Implement exponentiation mod n

--- problem4.go
+++ problem4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math"
 )
 
 //Function gcd uses Euclid's algorithm to compute the inverse of a number, mod m
@@ -30,6 +31,31 @@ func euclid(a, b int) (x int, y int) {
 	}
 }
 
+//exp returns a^{pow} mod n
+func exp(a, pow, n int) int {
+	square_iterations := int(math.Floor(math.Log2(float64(pow))))
+	//We need to square a "square_iterations" times
+	//then multiply by a^(pow-(2^square_iterations))
+
+	if square_iterations == 0 {
+		return 1
+	}
+
+	tmp := a
+	for i := 0; i < square_iterations; i++ {
+		tmp = (tmp * tmp) % n
+	}
+
+	//Now, tmp = a^(2^square_iterations)
+
+	//TODO make this more performant
+	//return tmp * exp(a, pow - int(math.Pow(2, float64(square_iterations))), n) % n
+	for i := 0; i < (pow - int(math.Pow(2, float64(square_iterations)))); i++ {
+		tmp = (tmp * a) % n
+	}
+	return tmp
+}
+
 //invert finds the modular inverse of an element, mod divisor
 func invert(element, divisor int) int {
 	g := gcd(element, divisor)
@@ -44,4 +70,8 @@ func main() {
 	log.Print(gcd(10, 15))
 	log.Print(euclid(10, 15))
 	log.Print(invert(101, 102))
+
+	log.Print(exp(2, 10, 5))
+	log.Print(exp(3, 16, 3000))
+
 }
