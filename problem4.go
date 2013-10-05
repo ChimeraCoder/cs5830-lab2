628b00349ce8310b5c449167f1dafad30ec5415f
Aditya Mukerjee <dev@chimeracoder.net>
2013-10-05 17:41:57 -0400

    Switch everything to math/big

--- problem4.go
+++ problem4.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"log"
 	"math"
+    "math/big"
     "math/rand"
 )
 
@@ -36,24 +36,50 @@ func euclid(a, b int) (x int, y int) {
 }
 
 //exp returns a^{pow} mod n
-func Exp(a, pow, n int) int {
-	if pow  == 0 {
-		return 1
+func Exp(a, pow, n big.Int) big.Int {
+    //check if pow == 0
+	if pow.Cmp(big.NewInt(0)) == 0 {
+		return *big.NewInt(1)
 	}
 
+    //log.Print("Done comapring")
 	//We need to square a "square_iterations" times
 	//then multiply by a^(pow-(2^square_iterations))
-	square_iterations := int(math.Floor(math.Log2(float64(pow))))
+    //Without math.Big, this would be equivalent to the following:
+	//square_iterations := int(math.Floor(math.Log2(float64(pow))))
+    square_iterations := pow.BitLen()
 
-	tmp := a
+
+	tmp := big.NewInt(1)
 	for i := 0; i < square_iterations; i++ {
-		tmp = (tmp * tmp) % n
+        //tmp = tmp * tmp % n
+		tmp = tmp.Mul(tmp, tmp)
+        
+        tmp = tmp.Mod(tmp, &n)
 	}
+    //log.Print("Done iterating powers of 2")
 
 	//Now, tmp = a^(2^square_iterations)
 
 	//TODO make this more performant
-	return tmp * Exp(a, pow - int(math.Pow(2, float64(square_iterations))), n) % n
+
+
+    //how many multiplications have already been performed
+    multiplications_performed := big.NewInt(0)
+    multiplications_performed = multiplications_performed.Exp(big.NewInt(2), big.NewInt(int64(square_iterations)), nil)
+    
+    new_pow := big.NewInt(0)
+    new_pow = new_pow.Sub(&pow, multiplications_performed)
+
+
+    //Without math/big, this would be the following
+    //return tmp * Exp(a, new_pow, n) % n
+    result := big.NewInt(0)
+    //log.Printf("About to do recursive call on %v %v %v", a, *new_pow, n) 
+    *result = Exp(a, *new_pow, n)
+    result = result.Mul(tmp, result)
+    result = result.Mod(result, &n)
+    return *result
 }
 
 //invert finds the modular inverse of an element, mod divisor
@@ -66,7 +92,7 @@ func invert(element, divisor int) int {
 	return s / g
 }
 
-func MillerRabin(n, numTests int) bool {
+func MillerRabin(n int64, numTests int) bool {
     for i := 0; i < numTests; i++ {
         if (MillerRabinAux(n) == false) {
             return false
@@ -75,7 +101,7 @@ func MillerRabin(n, numTests int) bool {
     return true
 }
 
-func MillerRabinAux(n int) bool {
+func MillerRabinAux(n int64) bool {
     d := n-1
     s := 0
     for ; d%2 == 0; {
@@ -83,23 +109,45 @@ func MillerRabinAux(n int) bool {
         s++
     }
 
-    a := r.Intn(n-3) + 2 // returns vals between [2, n-2] incl.
-    x := Exp(a, d, n)
+    a := r.Int63n(n-3) + 2 // returns vals between [2, n-2] incl.
+    x := Exp(*big.NewInt(a), *big.NewInt(d), *big.NewInt(n))
 
 
-    if (x == 1) || (x == n-1) {
+    //Equivalent to
+    //if (x == 1) || (x == n-1) {
+    if (x.Cmp(big.NewInt(1)) == 0) || (x.Cmp(big.NewInt(n-1)) == 0) {
         return true
     }
+
     for i := 0; i < s-1; i++ {
-        x = Exp(x, 2, n)
-        if (x == 1) {
+        x = Exp(x, *big.NewInt(2), *big.NewInt(n))
+        if (x.Cmp(big.NewInt(1)) == 0) {
             return false
         }
-        if (x == n-1) {
+        if (x.Cmp(big.NewInt(n-1)) == 0) {
             return true
         }
     }
     return false
+}
+
+
+//RandomNBitNumber returns a random number with the specified number of bits
+func RandomNBitNumber(n int) int64{
+    lower_bound := int64(math.Pow(2, float64(n))) //Inclusive
+    upper_bound := int64(math.Pow(2, float64(n+1))) //Exclusive
+
+    return r.Int63n(upper_bound - lower_bound) + lower_bound
+}
+
+//RandomNBitPrime returns random prime numbers of the specified size
+func RandomNBitPrime(n int, certainty int) int64{
+    for {
+        n := RandomNBitNumber(n)
+        if MillerRabin(n, certainty){
+            return n
+        }
+    }
 }
 
 func main() {
--- problem4_test.go
+++ problem4_test.go
@@ -3,15 +3,18 @@ package main
 import (
 	"testing"
     "log"
+    "math/big"
 )
 
 func Test_Exponentiation(t *testing.T) {
-	if Exp(3, 16, 3000) != 2721 {
+
+	if e := Exp(*big.NewInt(3), *big.NewInt(16), *big.NewInt(3000)); e.Cmp(big.NewInt(2721)) != 0 {
 		t.Error("exp failed")
 	} else {
 		t.Log("first exp passed") // log some info if you want
 	}
-	if Exp(2, 10, 5) != 4 {
+	//if Exp(2, 10, 5) != 4 {
+	if e := Exp(*big.NewInt(2), *big.NewInt(10), *big.NewInt(5)); e.Cmp(big.NewInt(4)) != 0 {
 		t.Error("second exp failed")
 	}
 }
@@ -24,9 +27,7 @@ func Test_invert(t *testing.T) {
 
 func Test_MillerRabin(t *testing.T) {
 
-    
-    prime_tests := []int{23251, 999331, 115249, 479001599}
-
+    prime_tests := []int64{23251, 999331, 115249, 479001599}
 
     for _, prime := range prime_tests{
         if !MillerRabin(prime, 10){
@@ -42,7 +43,16 @@ func Test_MillerRabin(t *testing.T) {
             log.Printf("Succeeded on composite number %d", composite)
         }
     }
-
-    
 }
 
+
+/**
+func TestRandomNBitNumber(t *testing.T){
+    
+    bits := 62
+    number := RandomNBitNumber(bits)
+    if !(4611686018427387904 <= number &&  number < 9223372036854775808) {
+        t.Errorf("Attempting to generate a %d-bit number yielded %d", bits, number)
+    }
+}
+*/
