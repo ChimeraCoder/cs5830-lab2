d430962c7df06ae6e5d5c2d46f4e1866864160ec
Aditya Mukerjee <dev@chimeracoder.net>
2013-10-05 14:51:45 -0400

    Implement invert, which computes the modular inverse

--- problem4.go
+++ problem4.go
@@ -18,8 +18,28 @@ func gcd(a, m int) int {
 	return a
 }
 
-func main() {
+//euclid returns the numbers x, y such that ax + by = gcd(a, b)
+func euclid(a, b int) (x int, y int) {
+	for {
+		if b == 0 {
+			return 1, 0
+		}
+		q := a / b
+		r := a % b
+		s, t := euclid(b, r)
+		return t, s - q*t
+	}
+}
 
-	//This should be 3
+//invert finds the modular inverse of an element, mod divisor
+func invert(element, divisor int) (int, int) {
+	g := gcd(element, divisor)
+	s, t := euclid(element, divisor)
+	return s / g, t / g
+}
+
+func main() {
 	log.Print(gcd(10, 15))
+	log.Print(euclid(10, 15))
+	log.Print(invert(2, 5))
 }
