647c58d94dd1bf6223755f98c5e9a4c4671ddfcf
Aditya Mukerjee <dev@chimeracoder.net>
2013-10-06 15:10:07 -0400

    Implement function to generate random n-bit safe primes

--- problem4.go
+++ problem4.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "math/big"
-    "math/rand"
+	"math/big"
+	"math/rand"
 )
 
 // using constant for debugging; in production would use time.now()
@@ -37,21 +37,21 @@ func euclid(a, b int) (x int, y int) {
 //exp returns a^{pow} mod n
 func Exp(a, pow, n big.Int) big.Int {
 
-    result := big.NewInt(1)
-    tmp := &a
+	result := big.NewInt(1)
+	tmp := &a
 
-    for i := 0;  i < pow.BitLen(); i++{
-        bit := pow.Bit(i)
-        if bit == 1 {
-            result.Mul(result, tmp)
-            result.Mod(result, &n)
-        }
+	for i := 0; i < pow.BitLen(); i++ {
+		bit := pow.Bit(i)
+		if bit == 1 {
+			result.Mul(result, tmp)
+			result.Mod(result, &n)
+		}
 
-        //This could be simplified by big.Exp, but we can't use that
-        tmp.Mul(tmp, tmp)
-        tmp.Mod(tmp, &n)
-    }
-    return *result
+		//This could be simplified by big.Exp, but we can't use that
+		tmp.Mul(tmp, tmp)
+		tmp.Mod(tmp, &n)
+	}
+	return *result
 }
 
 //invert finds the modular inverse of an element, mod divisor
@@ -64,67 +64,105 @@ func invert(element, divisor int) int {
 	return s / g
 }
 
-func MillerRabin(n int64, numTests int) bool {
-    for i := 0; i < numTests; i++ {
-        if (MillerRabinAux(n) == false) {
-            return false
-        }
-    }
-    return true
+//MillerRabin checks if the number is prime, using the Miller-Rabin test
+//It may return false positives (ie, incorrectly identify a composite number as prime)
+//It will never incorrectly reject a prime number as composite
+//Higer values of numTests will decrease the chance of a false positive
+func MillerRabin(n big.Int, numTests int) bool {
+	for i := 0; i < numTests; i++ {
+		if MillerRabinAux(n) == false {
+			return false
+		}
+	}
+	return true
 }
 
-func MillerRabinAux(n int64) bool {
-    d := n-1
-    s := 0
-    for ; d%2 == 0; {
-        d = d / 2
-        s++
-    }
+func MillerRabinAux(n big.Int) bool {
+	d := big.NewInt(0)
+	d = d.Sub(&n, big.NewInt(1))
 
-    a := r.Int63n(n-3) + 2 // returns vals between [2, n-2] incl.
-    x := Exp(*big.NewInt(a), *big.NewInt(d), *big.NewInt(n))
+	s := big.NewInt(0)
 
+	result := big.NewInt(0)
 
-    //Equivalent to
-    //if (x == 1) || (x == n-1) {
-    if (x.Cmp(big.NewInt(1)) == 0) || (x.Cmp(big.NewInt(n-1)) == 0) {
-        return true
-    }
+	for {
+		if result := result.Mod(d, big.NewInt(2)); result.Cmp(big.NewInt(0)) == 0 {
+			break
+		}
 
-    for i := 0; i < s-1; i++ {
-        x = Exp(x, *big.NewInt(2), *big.NewInt(n))
-        if (x.Cmp(big.NewInt(1)) == 0) {
-            return false
-        }
-        if (x.Cmp(big.NewInt(n-1)) == 0) {
-            return true
-        }
-    }
-    return false
+		d = d.Div(d, big.NewInt(2)) //d = d/2
+		s.Add(s, big.NewInt(1))     // s++
+	}
+
+	//Without math.Big, the following would look like
+	//a := r.Int63n(n-3) + 2 // returns vals between [2, n-2] incl.
+	a := big.NewInt(0)
+	upper := big.NewInt(0)
+	upper = upper.Sub(&n, big.NewInt(3))
+	a = a.Rand(r, upper)
+	a = a.Add(a, big.NewInt(2))
+
+	x := Exp(*a, *d, n)
+
+	//Equivalent to
+	//if (x == 1) || (x == n-1) {
+	f := big.NewInt(0)
+	if (x.Cmp(big.NewInt(1)) == 0) || (x.Cmp(f.Sub(&n, big.NewInt(1))) == 0) {
+		return true
+	}
+
+	s_minus_one := big.NewInt(0)
+	s_minus_one = s_minus_one.Sub(s, big.NewInt(1))
+
+	for i := big.NewInt(0); i.Cmp(s_minus_one) == -1; i.Add(i, big.NewInt(1)) {
+		x = Exp(x, *big.NewInt(2), n)
+		if x.Cmp(big.NewInt(1)) == 0 {
+			return false
+		}
+
+		tmp := big.NewInt(0)
+		if x.Cmp(tmp.Sub(&n, big.NewInt(1))) == 0 {
+			return true
+		}
+	}
+	return false
 }
-
 
 //RandomNBitNumber returns a random number with the specified number of bits
-func RandomNBitNumber(n int64) big.Int{
+func RandomNBitNumber(n int64) big.Int {
 
-    bigN := big.NewInt(n-1)
-    bigN = bigN.Exp(big.NewInt(2), big.NewInt(n), nil)
+	bigN := big.NewInt(n - 1)
+	bigN = bigN.Exp(big.NewInt(2), big.NewInt(n), nil)
 
-    result := big.NewInt(0)
-    result = result.Rand(r, bigN)
-    
-    result.Add(result, bigN)
-    return *result
+	result := big.NewInt(0)
+	result = result.Rand(r, bigN)
+
+	result.Add(result, bigN)
+	return *result
 }
 
 //RandomNBitPrime returns random prime numbers of the specified size
 func RandomNBitPrime(n int64, certainty int) big.Int {
-    for {
-        result := RandomNBitNumber(n)
-        if MillerRabin(n, certainty){
-            return result
-        }
-    }
+	for {
+		result := RandomNBitNumber(n)
+		if MillerRabin(result, certainty) {
+			return result
+		}
+	}
+}
+
+//RandomNBitSafePrime is like RandomNBitPrime, except it is guaranteed to return
+//only safe primes
+func RandomNBitSafePrime(n int64, certainty int) big.Int {
+	for {
+		number := RandomNBitPrime(n, certainty)
+		other := big.NewInt(0)
+		other = other.Sub(&number, big.NewInt(1))
+		other = other.Div(other, big.NewInt(2))
+		if MillerRabin(*other, certainty) {
+			return number
+		}
+	}
 }
 
 func main() {
--- problem4_test.go
+++ problem4_test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"log"
+	"math/big"
 	"testing"
-    "log"
-    "math/big"
 )
 
 func Test_Exponentiation(t *testing.T) {
@@ -27,38 +27,58 @@ func Test_invert(t *testing.T) {
 
 func Test_MillerRabin(t *testing.T) {
 
-    prime_tests := []int64{23251, 999331, 115249, 479001599}
+	prime_tests := []int64{23251, 999331, 115249, 479001599}
 
-    for _, prime := range prime_tests{
-        if !MillerRabin(prime, 10){
-            t.Errorf("MillerRabin failed to detect primality of %d", prime)
-        } else {
-            log.Printf("Succeeded on prime %d", prime)
-        }
+	for _, prime := range prime_tests {
+		prime_big := big.NewInt(prime)
+		if !MillerRabin(*prime_big, 10) {
+			t.Errorf("MillerRabin failed to detect primality of %s", prime_big.String())
+		} else {
+			log.Printf("Succeeded on prime %s", prime_big.String())
+		}
 
-        composite := prime +2 
-        if MillerRabin(composite, 10) {
-            t.Errorf("MillerRabin claimed that composite number %d is prime", composite)
-        } else {
-            log.Printf("Succeeded on composite number %d", composite)
-        }
-    }
+		composite := prime + 2
+		composite_big := big.NewInt(composite)
+		if MillerRabin(*composite_big, 10) {
+			t.Errorf("MillerRabin claimed that composite number %d is prime", composite)
+		} else {
+			log.Printf("Succeeded on composite number %d", composite)
+		}
+	}
 }
 
+func TestRandomNBitNumber(t *testing.T) {
 
-func TestRandomNBitNumber(t *testing.T){
-    
-    number := RandomNBitNumber(62)
-    lower := big.NewInt(0)
-    upper := big.NewInt(0)
-    lower, s1 := lower.SetString("4611686018427387904", 10); 
-    upper, s2 := upper.SetString("9223372036854775808", 10)
-    
-    if !s1 || !s2 {
-        t.Errorf("Error creating bounds for test")
-    }
+	number := RandomNBitNumber(62)
+	lower := big.NewInt(0)
+	upper := big.NewInt(0)
+	lower, s1 := lower.SetString("4611686018427387904", 10)
+	upper, s2 := upper.SetString("9223372036854775808", 10)
 
-    if lower.Cmp(&number) > 0 || upper.Cmp(&number) != 1 {
-        t.Errorf("Attempting to generate a %d-bit number yielded %s", 62, number.String())
-    }
+	if !s1 || !s2 {
+		t.Errorf("Error creating bounds for test")
+	}
+
+	if lower.Cmp(&number) > 0 || upper.Cmp(&number) != 1 {
+		t.Errorf("Attempting to generate a %d-bit number yielded %s", 62, number.String())
+	}
+}
+
+func Test_RandomNBitSafePrime(t *testing.T) {
+	bits := int64(80)
+	number := RandomNBitSafePrime(bits, 10)
+
+	//If number is a safe prime, "other_prime" should be prime too
+
+	other_prime := big.NewInt(0)
+	other_prime = other_prime.Sub(&number, big.NewInt(1))
+	other_prime = other_prime.Div(other_prime, big.NewInt(2))
+
+	//This is equivalent to
+	//other_prime := (number - 1) / 2
+
+	if !MillerRabin(*other_prime, 20) {
+		t.Errorf("Error - %s is not a safe prime, as %s is composite", number.String(), other_prime.String())
+	}
+
 }
